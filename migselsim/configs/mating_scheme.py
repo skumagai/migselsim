01be4977bc4a58376701748aac3aea3f41bfc8b2
Seiji Kumagai <seiji.kumagai@gmail.com>
2012-04-20 18:39:37 -0400

    new config plugins for reproduction related parameters.

---
+++ migselsim/configs/mating_scheme.py
@@ -0,0 +1,50 @@
+# -*- mode: python; coding: utf-8; -*-
+
+from migselsim.configs import ConfigPlugin
+from migselsim.core import RandomMating, UNIFORM_DISTRIBUTION
+
+class MatingScheme(ConfigPlugin):
+    key = 'mating scheme'
+    requirement = 'required'
+    parent = 'population structure'
+    conflict = None
+    simple_entries = ('mating type', 'number of offspring per mating')
+
+    def main(self, value, parent, simulator):
+        MatingScheme.verifyParent(parent)
+        for item in value.iteritems():
+            [key, val] = item
+            if key in simple_entries:
+                key = key.replace(' ', '_')
+                self.__getattr__(key)(val, simulator)
+            else:
+                ConfigPlugin.action(key)(val,
+                                         MatingScheme.key,
+                                         simulator)
+
+    def mating_type(self, mating_type, simulator):
+        try:
+            mating_type = mating_type.lower()
+        except:
+            pass
+        if mating_type == 'random mating':
+            simulator.mating_type = RandomMating
+        else:
+            raise NotImplementedError
+
+    def number_of_offspring_per_mating(self, val, simulator):
+        try:
+            val = int(val)
+            simulator.number_of_offspring = val
+        except TypeError:
+            key = val[0]
+            params = [float(i) for i in val[1:]]
+            try:
+                key = key.lower()
+            except:
+                pass
+            if key == 'uniform':
+                simulator.number_of_offspring = \
+                    (UNIFORM_DISTRIBUTION, params[0], params[1])
+            else:
+                raise NotImplementedError
---
+++ migselsim/configs/offspring_sex.py
@@ -0,0 +1,53 @@
+# -*- mode: python; coding: utf-8; -*-
+
+from migselsim.configs import ConfigPlugin
+from migselsim.core import RandomMating, getRNG, UNIFORM_DISTRIBUTION, MALE, FEMALE
+
+class OffspringSex(ConfigPlugin):
+    key = 'offsprint sex'
+    requirement = 'required'
+    parent = 'mating scheme'
+    conflict = None
+    simple_entries = ('exact', 'by probability')
+
+    def main(self, value, parent, simulator):
+        OffspringSex.verifyParent(parent)
+        if 'exact' in value and 'by probability' in value:
+            # should use different exception.
+            raise TypeError
+        elif 'exact' in value:
+            simulator.sex_mode = self.exact(float(value['proportion']))
+        elif 'by probability':
+            simulator.sex_mode = (PROB_OF_MALES, float(value['proportion']))
+        else:
+            raise NotImplementedError
+
+    def exact(self, prop):
+        """Return generator function determining sex of offspring.
+
+        The sex ratio is exactly at `prop`
+        """
+        def mating_func():
+            current = MALE
+            while True:
+                if current == MALE:
+                    current = FEMALE
+                else:
+                    current = MALE
+                yield current
+
+        return mating_func
+
+    def by_probability(self, prop):
+        """Return generator function determining sex of offspring.
+
+        Offspring is male `prop` of time, and female 1 - `prop` of time.
+        """
+        def mating_func():
+            rng = getRNG()
+            while True:
+                sex = MALE
+                if rng.randUniform() > prop:
+                    sex = FEMALE
+                yield sex
+        return mating_func
