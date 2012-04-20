c4b8afdf6229ee596f47286c9b128dd7d9f45103
Seiji Kumagai <seiji.kumagai@gmail.com>
2012-04-20 18:43:58 -0400

    add migselsim.core.definition, which imports functions and constants from simuPOP namespace.

---
+++ migselsim/core/definition.py
@@ -0,0 +1,9 @@
+# -*- mode: python; coding: utf-8; -*-
+
+"""Import simuPOP's function and constant names."""
+
+# import functions
+from simPOP import RandomMating
+
+# import constants
+from simuPOP import MALE, FEMALE, UNIFORM_DISTRIBUTION, getRNG
