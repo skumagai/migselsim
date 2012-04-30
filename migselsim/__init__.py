4764384c9dd31c1547f7f1dabf753715f2d79414
Seiji Kumagai <seiji.kumagai@gmail.com>
2012-04-30 18:46:26 -0400

    make option handling properly works with argparse.

--- migselsim/__init__.py
+++ migselsim/__init__.py
@@ -2,24 +2,19 @@
 
 import sys
 
-from migselsim.commands import list_all_commands
+from migselsim.commands import all_commands
 from migselsim.baseparser import parser
 
-__version__ == 0.0.1
-
 def main():
-    options, args = parser.parse_args(sys.argv[1:])
-    if options.help and not args:
-        args = ['help']
-    if not args:
-        args = ['help']         # Fallback
+    # args = parser.parse_args(sys.argv[1:])
+    print all_commands
 
-    command = args[0].lower()
-    try:
-        commands = list_all_commands()
-        commands[command]().execute(args[1:])
-    except:
-        parser.error("Unknown command: `{}`".format(command))
+    # command = args[0].lower()
+    # try:
+    #     commands = list_all_commands()
+    #     commands[command]().execute(args[1:])
+    # except:
+    #     parser.error("Unknown command: `{}`".format(command))
 
 
 # from migselsim.configs import parse_config
