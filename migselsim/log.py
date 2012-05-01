36bda48595d083e69d88008b20f41d7f7b96536a
Seiji Kumagai <seiji.kumagai@gmail.com>
2012-05-01 17:48:47 -0400

    use colors only for ERROR/DEBUG/WARN etc, and not for messages.

--- migselsim/log.py
+++ migselsim/log.py
@@ -7,16 +7,16 @@ from sys import stdout, stderr
 # inspired by @utahta's pythonbrew.log
 
 class Color(object):
-    DEBUG = '\033[35m'
-    INFO = '\033[32m'
-    LOG = '\033[0m'
-    WARN = '\033[33m'
-    ERROR = '\033[31m'
     END = '\033[0m'
+    DEBUG = '\033[35mDEBUG' + END
+    INFO = '\033[32mINFO' + END
+    LOG = '\033[0mLOG' + END
+    WARN = '\033[33mWARN' + END
+    ERROR = '\033[31mERROR' + END
 
     @classmethod
     def _deco(cls, msg, color):
-        return '{}{}{}'.format(color, msg, cls.END)
+        return '{}: {}\n'.format(color, msg)
 
     @classmethod
     def debug(cls, msg):
@@ -42,31 +42,31 @@ class Logger(object):
 
     def debug(self, msg, to = stderr):
         try:
-            to.write(Color.debug('DEBUG: {}\n'.format(msg)))
+            to.write(Color.debug(msg))
         except Exception as e:
             self.error(e, stderr)
 
     def log(self, msg, to = stdout):
         try:
-            to.write(Color.log('{}\n'.format(msg)))
+            to.write(Color.log(msg))
         except Exception as e:
             self.error(e, stderr)
 
     def info(self, msg, to = stdout):
         try:
-            to.write(Color.info('INFO: {}\n'.format(msg)))
+            to.write(Color.info(msg))
         except Exception as e:
             self.error(e, stderr)
 
     def warn(self, msg, to = stderr):
         try:
-            to.write(Color.warn('WARN: {}\n'.format(msg)))
+            to.write(Color.warn(msg))
         except Exception as e:
             self.error(e, stderr)
 
     def error(self, msg, to = stderr):
         try:
-            to.write(Color.error('ERROR: {}\n'.format(msg)))
+            to.write(Color.error(msg))
         except Exception as e:
             self.error(e, stderr)
 
