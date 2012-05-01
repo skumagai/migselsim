76bf81ac7d8fc0935a15e3d2f76e4d805e5368a6
Seiji Kumagai <seiji.kumagai@gmail.com>
2012-05-01 13:22:30 -0400

    clean up main() by removing obsolate code and by simplifying the invocation of subcommands.

--- migselsim/__init__.py
+++ migselsim/__init__.py
@@ -6,26 +6,14 @@ from migselsim.commands import all_commands
 from migselsim.baseparser import parser
 
 def main():
-    # args = parser.parse_args(sys.argv[1:])
-    print all_commands
+    # special case. When no argument is given print help.
+    if len(sys.argv[1:]) == 0:
+        parser.parse_args(['-h'])
 
-    # command = args[0].lower()
-    # try:
-    #     commands = list_all_commands()
-    #     commands[command]().execute(args[1:])
-    # except:
-    #     parser.error("Unknown command: `{}`".format(command))
+    args = parser.parse_args(sys.argv[1:])
 
-
-# from migselsim.configs import parse_config
-    # sims = []
-    # try:
-    #     with open(sys.argv[1], 'r') as f:
-    #         stream = f.read()
-    #         sims.append(parse_config(stream))
-    #     for sim in sims:
-    #         pass
-    #         # sim.run()
-    # except IndexError:
-    #     sys.stderr.write("missing config file.")
-    #     sys.stderr.flush()
+    command = args.command.lower()
+    try:
+        all_commands[command].execute(args)
+    except Exception:
+        parser.error("Unknown command: `{}`".format(command))
