75ff81b15b51c827df9cee9a2b1a8a570eb6e345
Seiji Kumagai <seiji.kumagai@gmail.com>
2012-05-01 13:24:39 -0400

    remove 'usage' argument in argparse, because custom usage does not play nice when subcommands.

--- migselsim/baseparser.py
+++ migselsim/baseparser.py
@@ -1,6 +1,6 @@
 # -*- mode: python; coding: utf-8; -*-
 from argparse import ArgumentParser
 
-parser = ArgumentParser(usage = '%(prog)s [options] <command> ...')
+parser = ArgumentParser()
 command_parsers = parser.add_subparsers(title = 'available commands',
                                         dest = 'command')
