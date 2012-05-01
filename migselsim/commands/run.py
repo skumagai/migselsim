8050cac1d4d12398b09a0fd4e1f825c9bf1d745d
Seiji Kumagai <seiji.kumagai@gmail.com>
2012-05-01 16:28:42 -0400

    implement 'run' subcommand.

--- migselsim/commands/run.py
+++ migselsim/commands/run.py
@@ -1,8 +1,10 @@
 # -*- mode: python; coding: utf-8; -*-
 
 import sys
+import os.path
 
 from migselsim.commands import CommandPlugin
+from migselsim.baseparser import command_parsers, parser
 from migselsim.configs import parse_config
 from migselsim.log import logger
 
@@ -11,19 +13,33 @@ class Run(CommandPlugin):
 
     key = 'run'
     description = 'run simulations'
-    usage = '{} {} CONFFILE'
+    # registering command line arguments/options has to occur exactly once per
+    # class.  This can be most easily achieved at the time the class definition
+    # is first read by python interpreter.  Hence, the code is here and not in
+    # any class or instance method.
+    subparser = command_parsers.add_parser(key, help = 'description')
+    subparser.add_argument('conffile', nargs = '?')
 
-    def execute(self, options, args):
+    def execute(self, args):
+        conffile = args.conffile
 
-        if len(args) == 1:
-            self.parser.print_help()
+        # When no conffile is supplied, print usage and exit.
+        if conffile is None:
+            parser.parse_args([self.key, '--help'])
+
+        # if conffile is not file, print error message and exit.
+        if not os.path.isfile(conffile):
+            logger.error('File not found: `{}`'.format(conffile))
             return
 
-        # parse config and set up simulator
-        simulators = None
-        with open(args[2], 'r') as f:
-            simulators = parse_config(f)
+        try:
+            # start reading file.
+            with open(conffile, 'r') as f:
+                simulators = parse_config(f)
+        except IOError as finenotfound:
+            logger.error(finenotfound)
+        except Exception as e:
+            logger.error(e)
 
-        # perform simulations
         for simulator in simulators:
             simulator.run()
