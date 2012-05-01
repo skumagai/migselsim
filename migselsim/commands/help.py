ebb37598899e44fcf6f6e033f633c6d765a2a6d5
Seiji Kumagai <seiji.kumagai@gmail.com>
2012-05-01 15:44:41 -0400

    clean up.

--- migselsim/commands/help.py
+++ migselsim/commands/help.py
@@ -8,19 +8,22 @@ class Help(CommandPlugin):
     key = 'help'
     description = 'print help messages of available commands'
     subparser = command_parsers.add_parser(key, help = description)
-    subparser.add_argument('target', nargs='?',
+    subparser.add_argument('target',
+                           nargs='?',
                            default='all',
                            metavar='command')
 
     def execute(self, args):
 
-        if args.target.lower() == 'all':
+        target = args.target.lower()
+
+        if target == 'all':
             # default case: print help of all commands.
             parser.print_help()
             return
 
-        if args.target not in CommandPlugin.plugins:
-            parser.error('Unkown command: `{}`'.format(args.target))
+        if target not in CommandPlugin.plugins:
+            parser.error('Unkown command: `{}`'.format(target))
             return
 
         parser.parse_args([args.target, '--help'])
