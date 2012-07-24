cb073cc0d62ad2ee6cade5f2d2c435efbedde16d
Seiji Kumagai <seiji.kumagai@gmail.com>
2012-07-24 15:56:57 -0400

    implement migration.

---
+++ migselsim/configs/migration.py
@@ -0,0 +1,22 @@
+# -*- mode: python; coding: utf-8; -*-
+
+from migselsim.configs import ConfigRecipe
+from migselsim.configs.utils import get_list_of_values
+
+class MigrationMatrix(ConfigRecipe):
+    key = 'migration:matrix'
+
+    class Matrix(object):
+        """simple class preventing flattening of matrix into vector."""
+        def __init__(self, val):
+            self.val = val
+
+    @classmethod
+    def apply(cls, node):
+        mnode = node.descendent('matrix')
+        dim = len(mnode.children)
+        mat = []
+        for row in mnode.children:
+            mat.append( get_list_of_values(row))
+
+        return cls.Matrix(mat)
